3e463d10b5c9f0bca5da3363127c2cbb20e57276
wcraycroft <wcraycroft@gmail.com>
2019-03-28 15:20:18 -0700

    GCD????????????

--- src/GCDCalculator.java
+++ src/GCDCalculator.java
@@ -1,2 +1,37 @@
+import java.util.Scanner;
+
 public class GCDCalculator {
+
+    public static void main (String[]args) {
+
+        int numberA, numberB;
+        Scanner keyboard = new Scanner(System.in);
+
+        // Prompt user for whole number
+        System.out.println("This program calculates the GCD of two whole numbers.");
+
+        // Start of input loop
+        while(true) {
+
+            System.out.print("Please enter first number: ");
+            numberA = keyboard.nextInt();
+            System.out.print("Please enter second number: ");
+            numberB = keyboard.nextInt();
+
+            // Output GCDs calculated with both methods
+            System.out.println("GCD calculated recursively: " + recursivelyCalculateGCD(numberA, numberB));
+            System.out.println("GCD calculated iteratively: " + recursivelyCalculateGCD(numberA, numberB));
+
+        }
+
+    }
+
+    public static int recursivelyCalculateGCD(int numberA, int numberB) {
+        // Base case, a % b = 0, a is the GCD
+        if (numberA % numberB == 0)
+            return numberA;
+
+        // Otherwise return the GCD of b and a % b
+        return recursivelyCalculateGCD(numberB, numberA % numberB);
+    }
 }
--- src/Palindromes.java
+++ src/Palindromes.java
@@ -1,2 +1,69 @@
+import java.util.Scanner;
+
 public class Palindromes {
+
+    public static void main(String[] args) {
+
+        // Declarations
+        Scanner keyboard = new Scanner(System.in);
+        String input, modified;
+
+        // Start of input loop
+        while (true) {
+            // Prompt user for Palindrome string
+            System.out.print("Enter a word, phrase or sentence (or 'quit' to exit): ");
+            input = keyboard.nextLine();
+
+            if (input.equalsIgnoreCase("quit"))
+                break;
+
+            // Remove all non-alphanumeric characters
+            modified = input.replaceAll("[^\\w\\s]", "");
+            // Remove all numbers and underscores
+            modified = modified.replaceAll("0123456789_", "");
+            // Remove all spaces
+            modified = modified.replaceAll(" ", "");
+            // Set remaining character to lower case
+            modified = modified.toLowerCase();
+
+            // Output the original string and whether or not it is a palindrome
+            System.out.printf("\"%s\" is%s a palindrome (recursive method used).\n", input,
+                    recursiveTestPalindrome(modified) ? "" : " not");
+
+            System.out.printf("\"%s\" is%s a palindrome (iterative method used).\n\n", input,
+                    iterativeTestPalindrome(modified) ? "" : " not");
+
+            //System.out.println(modified);
+
+        } // end of input loop
+    }
+
+    // This recursive method returns True if the input String is a Palindrome (reads forwards and backwards)
+    public static boolean recursiveTestPalindrome(String text) {
+        // Base condition: if string length is <2 return true
+        if (text.length() < 2)
+            return true;
+
+        // Test if first and last character match
+        if (text.substring(0,1).equals(text.substring(text.length() - 1))) {
+            // call recursively for remaining chars
+            return recursiveTestPalindrome(text.substring(1, text.length() - 1));
+        }
+        // Else, it is not a palindrome
+        return false;
+    }
+
+    // This iterative method returns True if the input String is a Palindrome (reads forwards and backwards)
+    public static boolean iterativeTestPalindrome(String text) {
+         // Loop through first half of string
+        for (int i = 0; i < text.length() / 2; i++) {
+            // Test if char at index i is equals to it counterpart at last char - i
+            if (!text.substring(i, i + 1).equals(
+                    text.substring(text.length() - (i + 1), text.length() - i)))
+                // If not equal, return false
+                return false;
+        }
+        // All characters match, return true
+        return true;
+    }
 }
